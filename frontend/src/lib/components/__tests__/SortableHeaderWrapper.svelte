dc57c44d02c8ccc0c114ab9b15a04018a241b921
lugassawan <lugassawan@gmail.com>
2026-03-05 23:19:12 +0700

    feat: add SortableHeader component for accessible table sorting

---
+++ frontend/src/lib/components/__tests__/SortableHeaderWrapper.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import SortableHeader from "../SortableHeader.svelte";
+
+let {
+  label = "Name",
+  field = "name",
+  currentSort = "",
+  ascending = true,
+  onclick = () => {},
+}: {
+  label?: string;
+  field?: string;
+  currentSort?: string;
+  ascending?: boolean;
+  onclick?: (field: string) => void;
+} = $props();
+</script>
+
+<table>
+  <thead>
+    <tr>
+      <SortableHeader {label} {field} {currentSort} {ascending} {onclick} />
+    </tr>
+  </thead>
+</table>
